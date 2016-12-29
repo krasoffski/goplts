d4c97d3ca130eb43b1269b0df75661da90bf8da3
Yury Krasouski <krasoffski@gmail.com>
2016-12-29 22:17:20 +0300

    Updated soluton for gif for task 1.6.

--- gopl/ch01/ex01.05/lissajous2.go
+++ gopl/ch01/ex01.05/lissajous2.go
@@ -1,4 +1,4 @@
-// lissajous2 solution for task 1.5
+// lissajous2 solution for task 1.6
 package main
 
 import (
@@ -13,13 +13,6 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.Black, color.RGBA{0, 128, 0, 255}}
-
-const (
-	blackIndex = 0
-	greenIndex = 1
-)
-
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	lissajous(os.Stdout)
@@ -27,23 +20,26 @@ func main() {
 
 func lissajous(out io.Writer) {
 	const (
+		colours = 128
 		cycles  = 5
 		res     = 0.001
 		size    = 200
 		nframes = 64
 		delay   = 8
 	)
+
 	freq := rand.Float64() * 3.0
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0
 	for i := 0; i < nframes; i++ {
+		palette := []color.Color{color.Black}
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
+		img := image.NewPaletted(rect, append(palette, rainbow()))
+
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				greenIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), 1)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
@@ -53,4 +49,18 @@ func lissajous(out io.Writer) {
 	if err != nil {
 		log.Fatalf("error encoding gif image: %v", err)
 	}
+}
+
+func rainbow() color.Color {
+
+	var vibgyor = []color.Color{
+		color.RGBA{0x94, 0x00, 0xD3, 0xFF}, // violet
+		color.RGBA{0x4B, 0x00, 0x82, 0xFF}, // indigo
+		color.RGBA{0x00, 0x00, 0xFF, 0xFF}, // blue
+		color.RGBA{0x00, 0xFF, 0x00, 0xFF}, // green
+		color.RGBA{0xFF, 0xFF, 0x00, 0xFF}, // yellow
+		color.RGBA{0xFF, 0x7F, 0x00, 0xFF}, // orange
+		color.RGBA{0xFF, 0x00, 0x00, 0xFF}, // red
+	}
+	return vibgyor[rand.Intn(len(vibgyor))]
 }
