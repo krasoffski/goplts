2a84441ecf16fd6c5aaf996a56a65f1245cbcce6
Yury Krasouski <krasoffski@gmail.com>
2016-12-30 14:33:38 +0300

    Added solution for fetch task 1.9.

--- gopl/ch01/fetch/fetch.go
+++ gopl/ch01/fetch/fetch.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"net/http"
@@ -8,9 +9,26 @@ import (
 	"strings"
 )
 
-var httpPrefix = "http://"
+type strslice []string
 
-func fetch(w io.Writer) {
+func (s *strslice) String() string {
+	return fmt.Sprintf("%s", *s)
+}
+
+func (s *strslice) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+const httpPrefix = "http://"
+
+var (
+	urls strslice
+	body bool
+	code bool
+)
+
+func fetch() {
 	urls := os.Args[1:]
 	if len(urls) == 0 {
 		fmt.Fprintln(os.Stderr, "error: no urls are spcified")
@@ -37,6 +55,33 @@ func fetch(w io.Writer) {
 	}
 }
 
+func fetchURL(w io.Writer, url string, body bool, code bool) error {
+
+	if !strings.HasPrefix(url, httpPrefix) {
+		url = httpPrefix + url
+	}
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error getting %s: %s", url, err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	if code {
+		fmt.Printf("%3d %s", resp.StatusCode, url)
+	}
+	return nil
+}
+
+func init() {
+	flag.Var(&urls, "url", "url to fetch, multiple allowed")
+	flag.BoolVar(&body, "body", false, "print responce body")
+	flag.BoolVar(&code, "code", true, "print responce statuc code for url")
+}
+
 func main() {
-	fetch(os.Stdout)
+	flag.Parse()
+	fetch()
 }
