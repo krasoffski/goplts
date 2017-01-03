6e7412f64f69e02030906765f43af29fb3e2fe09
Yury Krasouski <krasoffski@gmail.com>
2017-01-03 16:45:41 +0300

    Preporation for coloring max/min.

--- gopl/ch03/surface/surface.go
+++ gopl/ch03/surface/surface.go
@@ -6,9 +6,9 @@ import (
 )
 
 const (
-	width      = 1200
-	height     = 640
-	cells      = 200
+	width      = 600
+	height     = 300
+	cells      = 100
 	xyrange    = 30.0
 	xyscale    = width / 2 / xyrange
 	multiplier = 0.4
@@ -23,7 +23,6 @@ var (
 
 func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: green; fill: black; stroke-width: 0.7' "+
 		"width='%d' height='%d'>\n", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
@@ -31,7 +30,8 @@ func main() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' "+
+				"style='stroke:green; fill:rgb(0,0,0); stroke-width:0.7'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
