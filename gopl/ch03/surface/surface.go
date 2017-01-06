94670c3d4a199b09ba2a42934c24108d4d3e1f75
Yury Krasouski <yury_krasouski@epam.com>
2017-01-06 11:02:08 +0300

    Moved colormap function to gomill lib.

--- gopl/ch03/surface/surface.go
+++ gopl/ch03/surface/surface.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/krasoffski/gomill/colormap"
 )
 
 const (
@@ -21,58 +23,26 @@ var (
 	cos30 = math.Cos(angle)
 )
 
-type colour struct {
-	r, g, b float64
-}
-
-func colorize(z, zmin, zmax float64) colour {
-	var dz float64
-	c := colour{1.0, 1.0, 1.0}
-	if z < zmin {
-		z = zmin
-	}
-	if z > zmax {
-		z = zmax
-	}
-	dz = zmax - zmin
-
-	if z < (zmin + 0.25*dz) {
-		c.r = 0
-		c.g = 4 * (z - zmin) / dz
-	} else if z < (zmin + 0.5*dz) {
-		c.r = 0
-		c.b = 1 + 4*(zmin+0.25*dz)/dz
-	} else if z < (zmin+0.75*dz)/dz {
-		c.r = 4 * (z - zmin - 0.5*dz) / dz
-		c.b = 0
-	} else {
-		c.g = 1 + 4*(zmin+0.75*dz-z)/dz
-		c.b = 0
-	}
-	c.r *= 100
-	c.g *= 100
-	c.b *= 100
-	return c
-}
-
 func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"width='%d' height='%d'>\n", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay, ac := corner(i+1, j)
+			ax, ay, az := corner(i+1, j)
 			bx, by, _ := corner(i, j)
 			cx, cy, _ := corner(i, j+1)
 			dx, dy, _ := corner(i+1, j+1)
+			// FIXME: check svg rgb
+			r, g, b := colormap.Scale(az, -0.2, +0.2)
 			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' "+
 				"style='stroke:green; fill:rgb(%g%%,%g%%,%g%%); stroke-width:0.7'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy, ac.r, ac.g, ac.b)
+				ax, ay, bx, by, cx, cy, dx, dy, r*100, g*100, b*100)
 		}
 	}
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64, colour) {
+func corner(i, j int) (float64, float64, float64) {
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
@@ -81,7 +51,7 @@ func corner(i, j int) (float64, float64, colour) {
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 
-	return sx, sy, colorize(z, -0.25, +0.25)
+	return sx, sy, z
 }
 
 func f1(x, y float64) float64 {
