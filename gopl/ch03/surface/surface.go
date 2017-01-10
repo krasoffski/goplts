6e93f4c332fc1b36fe123e3feb8b250525ed04ab
Yury Krasouski <krasoffski@gmail.com>
2017-01-10 23:19:59 +0300

    Broken. Creating max min values for f(x,y)

--- gopl/ch03/surface/surface.go
+++ gopl/ch03/surface/surface.go
@@ -23,22 +23,30 @@ var (
 	cos30 = math.Cos(angle)
 )
 
+type point struct {
+	x, y, z float64
+}
+
+func (p *point) real() bool {
+	return !math.IsNaN(p.z)
+}
+
 func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"width='%d' height='%d'>\n", width, height)
+
+	points := make([]point, 0)
+	var min, max float64
+
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay, fa := corner(i+1, j)
-			bx, by, _ := corner(i, j)
-			cx, cy, _ := corner(i, j+1)
-			dx, dy, _ := corner(i+1, j+1)
-			if math.IsNaN(ax) || math.IsNaN(ay) ||
-				math.IsNaN(bx) || math.IsNaN(by) ||
-				math.IsNaN(cx) || math.IsNaN(cy) ||
-				math.IsNaN(dx) || math.IsNaN(dy) {
-				continue
-			}
-			color := htcmap.AsStr(fa, -0.15, +0.15)
+			ax, ay, az := calculate(i+1, j)
+			bx, by, bz := calculate(i, j)
+			cx, cy, cz := calculate(i, j+1)
+			dx, dy, dz := calculate(i+1, j+1)
+
+			color := htcmap.AsStr((bz+dz)/2, -0.13, +0.13)
+
 			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' "+
 				"style='stroke:green; fill:%s; stroke-width:0.7'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy, color)
@@ -47,16 +55,19 @@ func main() {
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64, float64) {
+func calculate(i, j int) (float64, float64, float64) {
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	z := f1(x, y)
+	return x, y, z
+}
 
+func transform(p point) (float64, float64) {
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 
-	return sx, sy, z
+	return sx, sy
 }
 
 func f1(x, y float64) float64 {
