3eca6cd6f3b30fe36e0299d9d48d2495c22cf112
Yury Krasouski <krasoffski@gmail.com>
2017-01-24 14:53:15 +0300

    Updated docs and added check for len of url params.

--- gopl/ch03/surface/surface.go
+++ gopl/ch03/surface/surface.go
@@ -13,15 +13,16 @@ import (
 	"github.com/krasoffski/gomill/htcmap"
 )
 
-// Settings represents required configuration for surface drawning.
+// Settings represents required configuration for 2D surface drawning.
 type Settings map[string]float64
 
-// Point represents dot on three dimensional system of coordinates.
+// Point represents dot on three dimensional system of coordinates with
+// coordinates X, Y, Z.
 type Point struct {
 	X, Y, Z float64
 }
 
-// Isometric transforms Point from 3 dimensional system to isometric.
+// Isometric transforms Point from 3 dimensional system to isometric one.
 func (p *Point) Isometric(s Settings) (float64, float64) {
 	sx := s["width"]/2 + (p.X-p.Y)*math.Cos(s["angle"])*s["xyscale"]
 	sy := s["height"]/2 + (p.X+p.Y)*math.Sin(s["angle"])*s["xyscale"] - p.Z*s["zscale"]
@@ -29,26 +30,31 @@ func (p *Point) Isometric(s Settings) (float64, float64) {
 }
 
 // NewPoint transform given cell with indexes i, j and Settings to coordinates
-// X and Y and executes function of two variables using created coordinates.
-// If successful, a pointer to Point returned or error in case function returns
-// non-real value like Nan, -Inf or +Inf.
+// X and Y and executes function of two variables using created coordinates to
+// get Z coordinate. If successful, a pointer to Point returned or error in case
+// function returns non-real value like Nan, -Inf or +Inf.
 func NewPoint(i, j int, s Settings) (*Point, error) {
 	// Transforming cell indexes to coordinates.
 	x := s["xyrange"] * (float64(i)/s["cells"] - 0.5)
 	y := s["xyrange"] * (float64(j)/s["cells"] - 0.5)
 	z := f1(x, y)
+	// Skipping non-real results of function.
 	if math.IsNaN(z) || math.IsInf(z, +1) || math.IsInf(z, -1) {
 		return nil, fmt.Errorf("error: function returned non real number")
 	}
 	return &Point{X: x, Y: y, Z: z}, nil
 }
 
-type Polygon struct {
+// Tetragon represents quadrangle with four points A, B, C, D.
+type Tetragon struct {
 	A, B, C, D *Point
 }
 
-func CreatePolygons(s Settings) []*Polygon {
-	polygons := make([]*Polygon, 0, int(s["cells"]*s["cells"]))
+// CreateTetragons produces slice of tetragons using given Settings. Tetragon
+// which contain Point with non-real Z is skipped (NewPoint error).
+func CreateTetragons(s Settings) []*Tetragon {
+	// Allocating required amount of memory for tetragons.
+	polygons := make([]*Tetragon, 0, int(s["cells"]*s["cells"]))
 
 	for i := 0; i < int(s["cells"]); i++ {
 		for j := 0; j < int(s["cells"]); j++ {
@@ -61,7 +67,7 @@ func CreatePolygons(s Settings) []*Polygon {
 			if aErr != nil || bErr != nil || cErr != nil || dErr != nil {
 				continue
 			}
-			polygons = append(polygons, &Polygon{A: a, B: b, C: c, D: d})
+			polygons = append(polygons, &Tetragon{A: a, B: b, C: c, D: d})
 		}
 	}
 	return polygons
@@ -70,14 +76,14 @@ func CreatePolygons(s Settings) []*Polygon {
 // Surface writes isometric representation of surface as svg image.
 func Surface(out io.Writer, s Settings) {
 
-	polygons := CreatePolygons(s)
+	tetragons := CreateTetragons(s)
 
-	if len(polygons) == 0 {
-		fmt.Fprintln(out, "error: no real points are exist")
+	if len(tetragons) == 0 {
+		fmt.Fprintln(out, "error: no real points exist")
 		return
 	}
-	min, max := polygons[0].A.Z, polygons[0].A.Z
-	for _, t := range polygons {
+	min, max := tetragons[0].A.Z, tetragons[0].A.Z
+	for _, t := range tetragons {
 		min = math.Min(min, t.A.Z)
 		max = math.Max(max, t.A.Z)
 	}
@@ -86,7 +92,7 @@ func Surface(out io.Writer, s Settings) {
 
 	colorRange := htcmap.NewRange(min, max)
 
-	for _, t := range polygons {
+	for _, t := range tetragons {
 
 		ax, ay := t.A.Isometric(s)
 		bx, by := t.B.Isometric(s)
@@ -95,22 +101,26 @@ func Surface(out io.Writer, s Settings) {
 
 		c := colorRange.AsStr(t.B.Z)
 		fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g' "+
-			"style='stroke:gray; fill:%s; stroke-width:0.5'/>\n",
-			ax, ay, bx, by, cx, cy, dx, dy, c)
+			"style='stroke:gray; fill:%s; stroke-width:%g'/>\n",
+			ax, ay, bx, by, cx, cy, dx, dy, c, s["stroke"])
 	}
 	fmt.Fprintln(out, "</svg>")
 }
 
 func handler(s Settings) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		values := r.URL.Query()
+		if len(values) > len(s) {
+			http.Error(w, "too many parameters", http.StatusBadRequest)
+			return
+		}
 		w.Header().Set("Content-Type", "image/svg+xml")
-		reqSettings := make(Settings)
+		reqSettings := make(Settings, len(s))
 
+		// Needed for per-request settings.
 		for k, v := range s {
 			reqSettings[k] = v
 		}
-
-		values := r.URL.Query()
 
 		for vName := range values {
 			if _, ok := reqSettings[vName]; !ok {
@@ -144,6 +154,7 @@ func main() {
 		"xyscale": 10,
 		"zscale":  128,
 		"angle":   math.Pi / 6,
+		"stroke":  0.5,
 	}
 	if *web {
 		http.HandleFunc("/", handler(settings))
