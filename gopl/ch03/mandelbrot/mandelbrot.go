85b98f2f5becd373c54e0f8a0f35cf5923868137
Yury Krasouski <krasoffski@gmail.com>
2017-01-27 00:04:26 +0300

    Playing with supersampling.

--- gopl/ch03/mandelbrot/mandelbrot.go
+++ gopl/ch03/mandelbrot/mandelbrot.go
@@ -5,7 +5,6 @@ import (
 	"image"
 	"image/color"
 	"image/png"
-	"math"
 	"math/cmplx"
 	"os"
 
@@ -14,18 +13,32 @@ import (
 
 func main() {
 	const (
-		xmin, ymin    = -2.2, -1.2
+		xmin, ymin    = -2.3, -1.2
 		xmax, ymax    = +1.2, +1.2
-		width, height = 1536, 1024
+		width, height = 4000, 3000
+		// step          = 1
 	)
 
+	// stepx := step * (xmax - xmin) / width
+	// stepy := step * (ymax - ymin) / height
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := float64(py)/height*(ymax-ymin) + ymin
+		y0 := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
-			x := float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
+			x0 := float64(px)/width*(xmax-xmin) + xmin
+			c := mandelbrot(complex(x0, y0))
+			// r0, g0, b0, _ := mandelbrot(complex(x0, y0)).RGBA()
+			// r1, g1, b1, _ := mandelbrot(complex(x0-stepx, y0-stepy)).RGBA()
+			// r2, g2, b2, _ := mandelbrot(complex(x0+stepx, y0-stepy)).RGBA()
+			// r3, g3, b3, _ := mandelbrot(complex(x0+stepx, y0+stepy)).RGBA()
+			// r4, g4, b4, _ := mandelbrot(complex(x0-stepx, y0+stepy)).RGBA()
+			// r := uint16(math.Sqrt(float64(r0*r0+r1*r1+r2*r2+r3*r3+r4*r4) / 5))
+			// g := uint16(math.Sqrt(float64(g0*g0+g1*g1+g2*g2+g3*g3+g4*g4) / 5))
+			// b := uint16(math.Sqrt(float64(b0*b0+b1*b1+b2*b2+b3*b3+b4*b4) / 5))
+			// r := uint16((r0 + r1 + r2 + r3 + r4) / 5)
+			// g := uint16((g0 + g1 + g2 + g3 + g4) / 5)
+			// b := uint16((b0 + b1 + b2 + b3 + b4) / 5)
+			img.Set(px, py, c)
 		}
 	}
 	if err := png.Encode(os.Stdout, img); err != nil {
@@ -41,9 +54,9 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		vAbs := cmplx.Abs(v)
-		if vAbs > 2 && n > 5 {
-			smooth := float64(n) + 1 - math.Log(math.Log(vAbs))/math.Log(2)
-			r, g, b := htcmap.AsUInt8(float64(smooth*contrast), 0, iterations)
+		if vAbs > 2 {
+			// smooth := float64(n) + 1 - math.Log(math.Log(vAbs))/math.Log(2)
+			r, g, b := htcmap.AsUInt8(float64(n*contrast), 0, iterations)
 			return color.RGBA{r, g, b, 255}
 		}
 	}
