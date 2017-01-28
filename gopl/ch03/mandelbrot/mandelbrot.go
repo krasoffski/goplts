323a97047c2604c02d0ad9dcc3805acf43420784
Yury Krasouski <krasoffski@gmail.com>
2017-01-28 19:22:11 +0300

    Working prototype of supersampling (2x2).

--- gopl/ch03/mandelbrot/mandelbrot.go
+++ gopl/ch03/mandelbrot/mandelbrot.go
@@ -13,32 +13,36 @@ import (
 
 func main() {
 	const (
-		xmin, ymin    = -2.3, -1.2
-		xmax, ymax    = +1.2, +1.2
-		width, height = 4000, 3000
-		// step          = 1
+		xmin, ymin        = -2.2, -1.2
+		xmax, ymax        = +1.2, +1.2
+		width, height     = 1536, 1024
+		widthSS, heightSS = width * 2, height * 2
 	)
 
-	// stepx := step * (xmax - xmin) / width
-	// stepy := step * (ymax - ymin) / height
+	xCord := func(x int) float64 {
+		return float64(x)/widthSS*(xmax-xmin) + xmin
+	}
+
+	yCord := func(y int) float64 {
+		return float64(y)/heightSS*(ymax-ymin) + ymin
+	}
+
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	for py := 0; py < height; py++ {
-		y0 := float64(py)/height*(ymax-ymin) + ymin
-		for px := 0; px < width; px++ {
-			x0 := float64(px)/width*(xmax-xmin) + xmin
-			c := mandelbrot(complex(x0, y0))
-			// r0, g0, b0, _ := mandelbrot(complex(x0, y0)).RGBA()
-			// r1, g1, b1, _ := mandelbrot(complex(x0-stepx, y0-stepy)).RGBA()
-			// r2, g2, b2, _ := mandelbrot(complex(x0+stepx, y0-stepy)).RGBA()
-			// r3, g3, b3, _ := mandelbrot(complex(x0+stepx, y0+stepy)).RGBA()
-			// r4, g4, b4, _ := mandelbrot(complex(x0-stepx, y0+stepy)).RGBA()
-			// r := uint16(math.Sqrt(float64(r0*r0+r1*r1+r2*r2+r3*r3+r4*r4) / 5))
-			// g := uint16(math.Sqrt(float64(g0*g0+g1*g1+g2*g2+g3*g3+g4*g4) / 5))
-			// b := uint16(math.Sqrt(float64(b0*b0+b1*b1+b2*b2+b3*b3+b4*b4) / 5))
-			// r := uint16((r0 + r1 + r2 + r3 + r4) / 5)
-			// g := uint16((g0 + g1 + g2 + g3 + g4) / 5)
-			// b := uint16((b0 + b1 + b2 + b3 + b4) / 5)
-			img.Set(px, py, c)
+	for py := 0; py < heightSS; py += 2 {
+		for px := 0; px < widthSS; px += 2 {
+			x0, x1, y0, y1 := xCord(px), xCord(px+1), yCord(py), yCord(py+1)
+			r0, g0, b0, _ := mandelbrot(complex(x0, y0)).RGBA()
+			r1, g1, b1, _ := mandelbrot(complex(x1, y0)).RGBA()
+			r2, g2, b2, _ := mandelbrot(complex(x0, y1)).RGBA()
+			r3, g3, b3, _ := mandelbrot(complex(x1, y1)).RGBA()
+
+			// r := uint16(math.Sqrt(float64(r0*r0+r1*r1+r2*r2+r3*r3) / 4))
+			// g := uint16(math.Sqrt(float64(g0*g0+g1*g1+g2*g2+g3*g3) / 4))
+			// b := uint16(math.Sqrt(float64(b0*b0+b1*b1+b2*b2+b3*b3) / 4))
+			r := uint16((r0 + r1 + r2 + r3) / 4)
+			g := uint16((g0 + g1 + g2 + g3) / 4)
+			b := uint16((b0 + b1 + b2 + b3) / 4)
+			img.Set(px/2, py/2, color.RGBA64{r, g, b, 0xFFFF})
 		}
 	}
 	if err := png.Encode(os.Stdout, img); err != nil {
