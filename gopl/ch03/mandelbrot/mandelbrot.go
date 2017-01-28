f859ea3b53590dea0f5ca3b001daabcce09cca29
Yury Krasouski <krasoffski@gmail.com>
2017-01-28 21:51:03 +0300

    Solution for task 3.6.

--- gopl/ch03/mandelbrot/mandelbrot.go
+++ gopl/ch03/mandelbrot/mandelbrot.go
@@ -11,38 +11,59 @@ import (
 	"github.com/krasoffski/gomill/htcmap"
 )
 
+const (
+	xmin, ymin    = -2.2, -1.2
+	xmax, ymax    = +1.2, +1.2
+	width, height = 1536, 1024
+	scale         = 2
+)
+
+func xCord(x int) float64 {
+	return float64(x)/(width*scale)*(xmax-xmin) + xmin
+}
+
+func yCord(y int) float64 {
+	return float64(y)/(height*scale)*(ymax-ymin) + ymin
+}
+
+func superSample(px, py int) color.RGBA64 {
+
+	var xCords, yCords [scale]float64
+	var subPixels [scale * scale]color.Color
+
+	// Single calculation of required coordinates for super sampling.
+	for i := 0; i < scale; i++ {
+		xCords[i] = xCord(px + i)
+		yCords[i] = yCord(py + i)
+	}
+
+	// Now instead of calculation coordinate only fetching required one.
+	for iy := 0; iy < scale; iy++ {
+		for ix := 0; ix < scale; ix++ {
+			subPixels[iy*scale+ix] = mandelbrot(complex(xCords[ix], yCords[iy]))
+		}
+	}
+
+	var rAvg, gAvg, bAvg float64
+
+	for _, c := range subPixels {
+		r, g, b, _ := c.RGBA()
+		// TODO: Figure out what is better type translation or scale*scale.
+		rAvg += float64(r) / (scale * scale)
+		gAvg += float64(g) / (scale * scale)
+		bAvg += float64(b) / (scale * scale)
+	}
+	return color.RGBA64{uint16(rAvg), uint16(gAvg), uint16(bAvg), 0xFFFF}
+}
+
 func main() {
-	const (
-		xmin, ymin        = -2.2, -1.2
-		xmax, ymax        = +1.2, +1.2
-		width, height     = 1536, 1024
-		widthSS, heightSS = width * 2, height * 2
-	)
-
-	xCord := func(x int) float64 {
-		return float64(x)/widthSS*(xmax-xmin) + xmin
-	}
-
-	yCord := func(y int) float64 {
-		return float64(y)/heightSS*(ymax-ymin) + ymin
-	}
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	for py := 0; py < heightSS; py += 2 {
-		for px := 0; px < widthSS; px += 2 {
-			x0, x1, y0, y1 := xCord(px), xCord(px+1), yCord(py), yCord(py+1)
-			r0, g0, b0, _ := mandelbrot(complex(x0, y0)).RGBA()
-			r1, g1, b1, _ := mandelbrot(complex(x1, y0)).RGBA()
-			r2, g2, b2, _ := mandelbrot(complex(x0, y1)).RGBA()
-			r3, g3, b3, _ := mandelbrot(complex(x1, y1)).RGBA()
 
-			// r := uint16(math.Sqrt(float64(r0*r0+r1*r1+r2*r2+r3*r3) / 4))
-			// g := uint16(math.Sqrt(float64(g0*g0+g1*g1+g2*g2+g3*g3) / 4))
-			// b := uint16(math.Sqrt(float64(b0*b0+b1*b1+b2*b2+b3*b3) / 4))
-			r := uint16((r0 + r1 + r2 + r3) / 4)
-			g := uint16((g0 + g1 + g2 + g3) / 4)
-			b := uint16((b0 + b1 + b2 + b3) / 4)
-			img.Set(px/2, py/2, color.RGBA64{r, g, b, 0xFFFF})
+	for py := 0; py < height*scale; py += scale {
+		for px := 0; px < width*scale; px += scale {
+			c := superSample(px, py)
+			img.Set(px/scale, py/scale, c)
 		}
 	}
 	if err := png.Encode(os.Stdout, img); err != nil {
