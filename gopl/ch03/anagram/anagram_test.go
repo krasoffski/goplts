8d854e2f5812c88b98d99456df40bd25bad25b07
Yury Krasouski <krasoffski@gmail.com>
2017-02-13 18:19:03 +0300

    Added bench tests for task 3.12

---
+++ gopl/ch03/anagram/anagram_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+var FIRST = "juswdmgrxqfknpboehlictazyv"
+var SECOND = "dshzugilpvayqjocnwrkfbemtx"
+
+func BenchmarkAnagramDict(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		isAnagram(FIRST, SECOND)
+	}
+}
+
+func BenchmarkAnagramSortRunes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		isAnagram2(FIRST, SECOND, sortString)
+	}
+}
+
+func BenchmarkAnagramSortJoin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		isAnagram2(FIRST, SECOND, sortString2)
+	}
+}
