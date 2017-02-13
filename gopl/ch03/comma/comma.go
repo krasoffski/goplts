43b27e2b5cbdc3bf4f6b3b3571f4c1d7d43b3fd7
Yury Krasouski <krasoffski@gmail.com>
2017-02-13 15:03:21 +0300

    Added solution for task 3.10

--- gopl/ch03/comma/comma.go
+++ gopl/ch03/comma/comma.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 )
 
 func main() {
 	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf("  %s\n", comma(os.Args[i]))
+		fmt.Printf("  %s\n", comma2(os.Args[i]))
 	}
 }
 
@@ -17,4 +18,21 @@ func comma(s string) string {
 		return s
 	}
 	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func comma2(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	skip := n % 3
+	var b bytes.Buffer
+	b.WriteString(s[:skip])
+	for i := skip; i < n-skip; i += 3 {
+		if i != 0 {
+			b.WriteString(",")
+		}
+		b.WriteString(s[i : i+3])
+	}
+	return b.String()
 }
