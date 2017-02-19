69a48e4790eb4fb831ea6ba06ebb33989221d520
Yury Krasouski <krasoffski@gmail.com>
2017-02-19 20:08:07 +0300

    Added draft for task 4.1

--- gopl/ch04/shabits/shabits.go
+++ gopl/ch04/shabits/shabits.go
@@ -8,6 +8,15 @@ import (
 
 const size = 4
 
+func bitCount(x byte) int {
+	num := 0
+	for x != 0 {
+		x = x & (x - 1)
+		num++
+	}
+	return num
+}
+
 func main() {
 
 	if len(os.Args) < 3 || len(os.Args[1]) != size*2 || len(os.Args[2]) != size*2 {
@@ -27,10 +36,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	var bits int
 	fmt.Printf("% x\n% x\n", s1, s2)
 	for i := 0; i < size; i++ {
+		bits += bitCount(s1[i] ^ s2[i])
 		fmt.Printf("%x ", s1[i]^s2[i])
 	}
 	fmt.Println("")
+	fmt.Println(bits)
 
 }
