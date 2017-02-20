88e9388db56cb9920bc412c671181fcec2f79d6f
Yury Krasouski <krasoffski@gmail.com>
2017-02-20 12:01:19 +0300

    Draft solution for task 4.2

--- gopl/ch04/shasum/shasum.go
+++ gopl/ch04/shasum/shasum.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"crypto/sha512"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -12,26 +16,26 @@ func main() {
 
 	shaType := flag.String("sha", "sha256", "type of sha")
 	flag.Parse()
-	// flag.Visit(func(f *flag.Flag) {
-	// 	val := f.Value.String()
-	// 	for _, ss := range shaTypes {
-	// 		if val == ss {
-	// 			return
-	// 		}
-	// 	}
-	// })
+	flag.Visit(func(f *flag.Flag) {
+		val := f.Value.String()
+		for _, ss := range shaTypes {
+			if val == ss {
+				return
+			}
+		}
+		fmt.Fprintf(os.Stderr, "error: invalid sha '%s'\n", *shaType)
+		os.Exit(1)
+	})
+
+	var buff bytes.Buffer
+	io.Copy(&buff, os.Stdin)
+
 	switch *shaType {
 	case "sha256":
-		fmt.Println("sha256")
-	// sha256.Sum256()
+		fmt.Printf("%x\n", sha256.Sum256(buff.Bytes()))
 	case "sha384":
-		fmt.Println("sha384")
-	// sha512.Sum384()
+		fmt.Printf("%x\n", sha512.Sum384(buff.Bytes()))
 	case "sha512":
-		fmt.Println("sha512")
-		// sha512.Sum512()
-	default:
-		fmt.Fprintf(os.Stderr, "error: invalid sha type '%s'\n", *shaType)
-		os.Exit(1)
+		fmt.Printf("%x\n", sha512.Sum512(buff.Bytes()))
 	}
 }
