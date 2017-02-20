9e3ad4a405b7a1f927565da3867a5da759b59a3b
Yury Krasouski <krasoffski@gmail.com>
2017-02-20 15:11:35 +0300

    Added interfaces for unit testing for task 4.2

--- gopl/ch04/shasum/shasum.go
+++ gopl/ch04/shasum/shasum.go
@@ -8,24 +8,33 @@ import (
 	"hash"
 	"io"
 	"os"
+	"strings"
 )
 
-var shaTypes = [...]string{"sha256", "sha384", "sha512"}
+var shaTypes = []string{"sha256", "sha384", "sha512"}
 
-func calcSHA(h hash.Hash) []byte {
-	if _, err := io.Copy(h, os.Stdin); err != nil {
-		fmt.Fprintf(os.Stderr, "error: unable to copy from stdin: %s", err)
+func calcSHA(h hash.Hash, r io.Reader) ([]byte, error) {
+	if _, err := io.Copy(h, r); err != nil {
+		return nil, err
+	}
+	return h.Sum(nil), nil
+}
+
+func shaFromStdin(h hash.Hash, w io.Writer) {
+	sum, err := calcSHA(h, os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error calculation sha: %s\n", err)
 		os.Exit(1)
 	}
-	return h.Sum(nil)
+	fmt.Fprintf(w, "%x\n", sum)
 }
 
 func main() {
-
-	shaType := flag.String("sha", "sha256", "type of sha")
+	shaType := flag.String("type", "sha256",
+		fmt.Sprintf("type of sha: %s", strings.Join(shaTypes, ",")))
 	flag.Parse()
 	flag.Visit(func(f *flag.Flag) {
-		val := f.Value.String()
+		val := strings.ToLower(f.Value.String())
 		for _, ss := range shaTypes {
 			if val == ss {
 				return
@@ -37,10 +46,10 @@ func main() {
 
 	switch *shaType {
 	case "sha256":
-		fmt.Printf("%x\n", calcSHA(sha256.New()))
+		shaFromStdin(sha256.New(), os.Stdout)
 	case "sha384":
-		fmt.Printf("%x\n", calcSHA(sha512.New384()))
+		shaFromStdin(sha512.New384(), os.Stdout)
 	case "sha512":
-		fmt.Printf("%x\n", calcSHA(sha512.New()))
+		shaFromStdin(sha512.New(), os.Stdout)
 	}
 }
