ec39c1e6ffb1f49e1a51b6c1a9d6bf7ef60044b8
Yury Krasouski <krasoffski@gmail.com>
2017-02-28 15:17:42 +0300

    Added docs to task 4.12

--- gopl/ch04/xkcd/xkcd.go
+++ gopl/ch04/xkcd/xkcd.go
@@ -12,7 +12,8 @@ import (
 const URL = "http://xkcd.com"
 const INFO = "info.0.json"
 
-type ComicInfo struct {
+// Info represents xkcd comic information.
+type Info struct {
 	Alt        string
 	Day        string
 	Image      string
@@ -25,50 +26,71 @@ type ComicInfo struct {
 	Year       string
 }
 
-func FetchComicInfo(url string) (*ComicInfo, error) {
+// FetchInfo gets xkcd comic information as json and unpack one to info struct.
+func FetchInfo(url string) (*Info, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	var info ComicInfo
+	var info Info
 	if err := json.NewDecoder(resp.Body).Decode(&info); err != nil {
 		return nil, err
 	}
-	fmt.Printf("loaded %s\n", url)
 	return &info, nil
 }
 
+// Cache represents cache of xkcd comic information.
 type Cache struct {
 	CheckedAt time.Time
-	Comics    map[int]*ComicInfo
+	Comics    map[int]*Info
 	LastNum   int
-	// TTL       time.Duration
 }
 
-func NewCache() *Cache {
-	return &Cache{Comics: make(map[int]*ComicInfo)}
-}
+// Update get the latest xkcd comic from site and compares with the latest from
+// the Cache. If new comics are appeared they well be added to Cache. Parameter
+// 'force' allowes to reinitialize cache from the begginging.
+func (c *Cache) Update(force bool) error {
 
-func (c *Cache) Update(newLast int, force bool) error {
-	for i := c.LastNum + 1; i <= newLast; i++ {
-		// TODO: think about _, ok := c.Comics[i] and moving force to high level
-		if ok := c.Comics[i]; !force && ok != nil {
+	newLast, err := FetchInfo(URL + "/" + INFO)
+	if err != nil {
+		return fmt.Errorf("check last error: %s", err)
+	}
+
+	// TODO: think about moving update to function.
+	if force || c.Comics == nil {
+		// TODO: don't like this hack.
+		c.LastNum = 0
+		c.Comics = make(map[int]*Info, newLast.Num)
+	}
+
+	for i := c.LastNum + 1; i <= newLast.Num; i++ {
+
+		if ok := c.Comics[i]; ok != nil {
 			continue
 		}
 		url := fmt.Sprintf("%s/%d/%s", URL, i, INFO)
-		info, err := FetchComicInfo(url)
+		info, err := FetchInfo(url)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "xkcd error for %s: %s\n", url, err)
+			fmt.Fprintf(os.Stderr, "update error: %s, %s\n", url, err)
 			continue
 		}
-		c.Comics[i] = info
+		c.Comics[i] = info // no-thread safe
 	}
 	c.CheckedAt = time.Now()
-	c.LastNum = newLast
+	c.LastNum = newLast.Num
 	return nil
 }
 
+// Load reads cache represented as JSON from Reader and unmarshal them to Cache.
+func (c *Cache) Load(r io.Reader) error {
+	if err := json.NewDecoder(r).Decode(c); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Dump writes Cache marshaled as JSON to Writer.
 func (c *Cache) Dump(w io.Writer) error {
 	if err := json.NewEncoder(w).Encode(c); err != nil {
 		return err
@@ -76,9 +98,7 @@ func (c *Cache) Dump(w io.Writer) error {
 	return nil
 }
 
-func (c *Cache) Load(r io.Reader) error {
-	if err := json.NewDecoder(r).Decode(c); err != nil {
-		return err
-	}
-	return nil
+// NewCache creates new pointer to Cache structure and initializes internal map.
+func NewCache() *Cache {
+	return &Cache{Comics: make(map[int]*Info)}
 }
