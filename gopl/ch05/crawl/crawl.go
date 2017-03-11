4aa3c7d454a0eca2249fcae34081d948f6c063f0
Yury Krasouski <krasoffski@gmail.com>
2017-03-11 23:35:02 +0300

    [5.13] Fixed host checking.

--- gopl/ch05/crawl/crawl.go
+++ gopl/ch05/crawl/crawl.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 )
 
-var startURLs []string
+var initial []string
 
 func breadFirst(f func(string) []string, worklist []string) {
-	copy(startURLs, worklist)
+	initial = append(initial, worklist...)
 	seen := make(map[string]bool)
 	for len(worklist) > 0 {
 		items := worklist
@@ -17,7 +18,7 @@ func breadFirst(f func(string) []string, worklist []string) {
 		for _, item := range items {
 			if !seen[item] {
 				seen[item] = true
-				if len(seen) > 10 {
+				if len(seen) > 20 {
 					return
 				}
 				worklist = append(worklist, f(item)...)
@@ -26,11 +27,22 @@ func breadFirst(f func(string) []string, worklist []string) {
 	}
 }
 
-func crawl(url string) []string {
-	fmt.Println(url)
-	list, err := Extract(url)
+func crawl(u string) []string {
+	fmt.Println(u)
+	list, err := Extract(u)
 	if err != nil {
 		log.Panic(err)
+	}
+
+	for _, link := range list {
+		linkParsed, _ := url.Parse(link)
+		for _, r := range os.Args[1:] {
+			startParsed, _ := url.Parse(r)
+			if linkParsed.Host == startParsed.Host {
+				fmt.Println(">>> ", link)
+
+			}
+		}
 	}
 	return list
 }
