62cff5b91155809d025126e866f516a39cf76e2e
Yury Krasouski <krasoffski@gmail.com>
2017-03-12 22:31:06 +0300

    [5.13] Small refactoring.

--- gopl/ch05/crawl/crawl.go
+++ gopl/ch05/crawl/crawl.go
@@ -33,18 +33,24 @@ func crawl(u string) []string {
 	if err != nil {
 		log.Panic(err)
 	}
+	forRelative(list, printRelative)
+	return list
+}
 
-	for _, link := range list {
+func forRelative(links []string, f func(*url.URL)) {
+	for _, link := range links {
 		linkParsed, _ := url.Parse(link)
 		for _, r := range os.Args[1:] {
 			startParsed, _ := url.Parse(r)
 			if linkParsed.Host == startParsed.Host {
-				fmt.Println(">>> ", link)
-
+				f(linkParsed)
 			}
 		}
 	}
-	return list
+}
+
+func printRelative(u *url.URL) {
+	fmt.Println(">>> ", u.Path)
 }
 
 func main() {
