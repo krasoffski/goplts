e37fb4c02531f59b17454e4f116e15014e1bf3a1
Yury Krasouski <krasoffski@gmail.com>
2017-03-13 18:53:51 +0300

    [5.13] Solution is added.

--- gopl/ch05/crawl/crawl.go
+++ gopl/ch05/crawl/crawl.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	// DOWNLOADPATH is relative path of $CWD to store pages.
+	DOWNLOADPATH = "cache"
+	// INDEXNAME is path to store pages.
+	INDEXNAME = "index.html"
 )
 
 var initial []string
@@ -19,6 +31,7 @@ func breadFirst(f func(string) []string, worklist []string) {
 			if !seen[item] {
 				seen[item] = true
 				if len(seen) > 20 {
+					// fmt.Println(seen)
 					return
 				}
 				worklist = append(worklist, f(item)...)
@@ -28,21 +41,24 @@ func breadFirst(f func(string) []string, worklist []string) {
 }
 
 func crawl(u string) []string {
-	fmt.Println(u)
+	// fmt.Println(u)
 	list, err := Extract(u)
 	if err != nil {
 		log.Panic(err)
 	}
-	forRelative(list, printRelative)
+	forRelative(list, printRelative, saveRelative)
 	return list
 }
 
-func forRelative(links []string, f func(*url.URL)) {
+func forRelative(links []string, callbacks ...func(*url.URL)) {
 	for _, link := range links {
 		linkParsed, _ := url.Parse(link)
 		for _, r := range os.Args[1:] {
 			startParsed, _ := url.Parse(r)
-			if linkParsed.Host == startParsed.Host {
+			if linkParsed.Host != startParsed.Host {
+				continue
+			}
+			for _, f := range callbacks {
 				f(linkParsed)
 			}
 		}
@@ -50,7 +66,45 @@ func forRelative(links []string, f func(*url.URL)) {
 }
 
 func printRelative(u *url.URL) {
-	fmt.Println(">>> ", u.Path)
+	fmt.Println(u)
+}
+
+func saveRelative(u *url.URL) {
+	var dirPath, filePath string
+
+	pagePath, pageName := path.Split(u.Path)
+
+	// NOTE: workaround for paths.Split and URL like https://golang.org/pkg/fmt
+	if strings.HasSuffix(pageName, ".html") {
+		dirPath = filepath.Join(DOWNLOADPATH, u.Host, pagePath)
+		filePath = filepath.Join(dirPath, pageName)
+	} else {
+		dirPath = filepath.Join(DOWNLOADPATH, u.Host, pagePath, pageName)
+		filePath = filepath.Join(dirPath, INDEXNAME)
+	}
+
+	// TODO: add directory cache to skip already created ones and avoid syscalls here.
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	resp, err := http.Get(u.String())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	f, err := os.Create("./" + filePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	defer f.Close()
+
+	// w := bufio.NewWriter(out)
+	io.Copy(f, resp.Body)
 }
 
 func main() {
