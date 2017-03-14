996da4c68203ca310e9af54758bae5480f4d94ed
Yury Krasouski <krasoffski@gmail.com>
2017-03-14 16:54:45 +0300

    [5.14] Solution is added.

---
+++ gopl/ch05/breadfirst/breadfirst.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+	for _, prereq := range prereqs {
+		breadthFirst(popularity, prereq)
+	}
+
+	fmt.Println("Popularity summary:")
+	for k, v := range courses {
+		fmt.Printf("\t'%s' is precondition for %d course(s)\n", k, v)
+	}
+}
+
+var courses = make(map[string]int)
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func popularity(course string) []string {
+	courses[course]++
+	return prereqs[course]
+}
