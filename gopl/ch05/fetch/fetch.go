d393b9ff98b5c33a4b8e86309529132b7cfbcf99
Yury Krasouski <krasoffski@gmail.com>
2017-03-16 10:15:10 +0300

    [5.18] Added place holder.

---
+++ gopl/ch05/fetch/fetch.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+func init() {
+	log.SetFlags(0)
+}
+
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	// Close file, but prefer error from Copy, if any.
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+	return local, n, err
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("please, specify URL to fetch")
+	}
+	filename, copied, err := fetch(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%d bytes were copied to %s\n", copied, filename)
+}
