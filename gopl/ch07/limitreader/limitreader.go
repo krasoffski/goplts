4f8010e68b0f51ba2c4fa867a903db5044e25a6d
Yury Krasouski <krasoffski@gmail.com>
2017-03-26 08:02:50 +0300

    [7.5] Solution is added (from io package).

---
+++ gopl/ch07/limitreader/limitreader.go
@@ -0,0 +1,23 @@
+// https://golang.org/src/io/io.go
+package limitreader
+
+import "io"
+
+func LimitReader(r io.Reader, n int64) io.Reader { return &LimitedReader{r, n} }
+
+type LimitedReader struct {
+	R io.Reader // underlying reader
+	N int64     // max bytes remaining
+}
+
+func (l *LimitedReader) Read(p []byte) (n int, err error) {
+	if l.N <= 0 {
+		return 0, io.EOF
+	}
+	if int64(len(p)) > l.N {
+		p = p[0:l.N]
+	}
+	n, err = l.R.Read(p)
+	l.N -= int64(n)
+	return
+}
