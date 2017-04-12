c05af5869fab6ee27cd8cc04efc590a398765864
Yury Krasouski <krasoffski@gmail.com>
2017-04-12 17:26:15 +0300

    [7.11] add draft solution for update

--- gopl/ch07/http4/http4.go
+++ gopl/ch07/http4/http4.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 type dollars float32
@@ -12,17 +14,20 @@ func (d dollars) String() string {
 	return fmt.Sprintf("$%.2f", d)
 }
 
-type database map[string]dollars
+type database struct {
+	sync.Mutex
+	items map[string]dollars
+}
 
-func (db database) list(w http.ResponseWriter, req *http.Request) {
-	for item, price := range db {
+func (db *database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db.items {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
 }
 
-func (db database) price(w http.ResponseWriter, req *http.Request) {
+func (db *database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	if price, ok := db[item]; ok {
+	if price, ok := db.items[item]; ok {
 		fmt.Fprintf(w, "%s\n", price)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
@@ -30,9 +35,31 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (db *database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if _, ok := db.items[item]; !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	newPrice, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid price: %q\n", price)
+		return
+	}
+	db.Lock()
+	db.items[item] = dollars(newPrice)
+	db.Unlock() // Don't use defer to release lock earlier.
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-	db := database{"shoes": 50, "socks": 5}
+	db := database{items: map[string]dollars{"shoes": 50, "socks": 5}}
 	http.HandleFunc("/list", db.list)
 	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
