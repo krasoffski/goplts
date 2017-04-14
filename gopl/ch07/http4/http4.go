ddb92da7f14e7e3da2ef04d9ab897167f3104069
Yury Krasouski <krasoffski@gmail.com>
2017-04-14 16:45:32 +0300

    [7.12] removed bootstap themes

--- gopl/ch07/http4/http4.go
+++ gopl/ch07/http4/http4.go
@@ -17,7 +17,6 @@ var templ = template.Must(template.New("itemlist").Parse(`
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
