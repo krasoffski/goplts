8fcab3cb7a2e005e8b598716836c307bc2b7eda0
Yury Krasouski <krasoffski@gmail.com>
2017-04-23 16:16:08 +0300

    [7.13] Solution is added.

--- gopl/ch07/eval/ast.go
+++ gopl/ch07/eval/ast.go
@@ -1,5 +1,10 @@
 package eval
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // An Expr is an arithmetic expression.
 type Expr interface {
 	// Eval returns the value of this Expr in the environment env.
@@ -11,12 +16,24 @@ type Expr interface {
 // A Var identifies a variable, e.g., x.
 type Var string
 
+func (e Var) String() string {
+	return string(e)
+}
+
 // A literal is numeric constant.
 type literal float64
+
+func (e literal) String() string {
+	return fmt.Sprintf("%g", e)
+}
 
 type unary struct {
 	op rune // one of '+', '-'
 	x  Expr
+}
+
+func (e unary) String() string {
+	return fmt.Sprintf("(%c%s)", e.op, e.x)
 }
 
 // A binary represents a binary operator expression.
@@ -25,8 +42,25 @@ type binary struct {
 	x, y Expr
 }
 
+func (e binary) String() string {
+	return fmt.Sprintf("(%s%c%s)", e.x, e.op, e.y)
+}
+
 // A call represents a function call expression, e.g., six(x).
 type call struct {
 	fn   string
 	args []Expr
+}
+
+func (e call) String() string {
+	buf := &bytes.Buffer{}
+	fmt.Fprintf(buf, "%s(", e.fn)
+	for i, arg := range e.args {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		write(buf, arg)
+	}
+	buf.WriteByte(')')
+	return buf.String()
 }
