486c341f6495b7939191b680f1855326f5c1ae3c
Yury Krasouski <krasoffski@gmail.com>
2017-04-27 11:46:50 +0300

    [7.15] Aded draft solution.

--- gopl/ch07/eval/cmd/evel/main.go
+++ gopl/ch07/eval/cmd/evel/main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/krasoffski/goplts/gopl/ch07/eval"
 )
 
 func init() {
 	log.SetPrefix("eval: ")
+	log.SetFlags(0)
 }
 
 func main() {
-
-	expr, err := eval.Parse("min(sqrt(A), pow(y, 3))")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter formula: ")
+	text, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
+	}
+	// expr, err := eval.Parse("min(sqrt(A), pow(y, 3))")
+	expr, err := eval.Parse(text)
+	if err != nil {
+		log.Fatalln(err)
 	}
 	vars := make(map[eval.Var]bool)
 	if err := expr.Check(vars); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(expr.Eval(eval.Env{"A": 100, "y": 15}))
+	env := make(eval.Env)
+	for v := range vars {
+		var val float64
+		fmt.Printf("Enter %s: ", v)
+		_, err := fmt.Scanf("%f\n", &val)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		env[v] = val
+	}
+	fmt.Println(expr.Eval(env))
 }
