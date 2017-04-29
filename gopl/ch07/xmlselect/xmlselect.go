d4af4e5e1de4981c6ad87f3c752b3282c3eb71a6
Yury Krasouski <krasoffski@gmail.com>
2017-04-29 20:27:55 +0300

    [7.17] Parsing input.

--- gopl/ch07/xmlselect/xmlselect.go
+++ gopl/ch07/xmlselect/xmlselect.go
@@ -8,7 +8,28 @@ import (
 	"strings"
 )
 
+type Element struct {
+	Name  string
+	Attrs []Attr
+}
+
+func NewElement(name string) *Element {
+	e := new(Element)
+	e.Name = name
+	e.Attrs = make([]Attr, 0)
+	return e
+}
+
+type Attr struct {
+	Name, Value string
+}
+
 func main() {
+	input, err := parseInput(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "xmlselect: %v\n", err)
+		os.Exit(1)
+	}
 	dec := xml.NewDecoder(os.Stdin)
 	var stack []xml.StartElement
 	for {
@@ -25,11 +46,29 @@ func main() {
 		case xml.EndElement:
 			stack = stack[:len(stack)-1]
 		case xml.CharData:
-			if containsAll(stack, os.Args[1:]) {
+			if containsAll(stack, input) {
 				printStack(stack, tok)
 			}
 		}
 	}
+}
+
+func parseInput(input []string) (elements []*Element, err error) {
+	var e *Element
+	for i, s := range input {
+		if strings.Contains(s, "=") {
+			if i > 0 {
+				fmt.Printf("%d, %s\n", i, s)
+				a := strings.SplitN(s, "=", 2)
+				e.Attrs = append(e.Attrs, Attr{a[0], a[1]})
+			} else {
+				return nil, fmt.Errorf("attribute before tag name: %s", s)
+			}
+		}
+		e = NewElement(s)
+		elements = append(elements, e)
+	}
+	return
 }
 
 func printStack(st []xml.StartElement, tok interface{}) {
@@ -40,16 +79,16 @@ func printStack(st []xml.StartElement, tok interface{}) {
 	fmt.Printf("%s: %s\n", strings.Join(names, " "), tok)
 }
 
-func containsAll(st []xml.StartElement, ss []string) bool {
+func containsAll(stack []xml.StartElement, search []*Element) bool {
 
-	for len(ss) <= len(st) {
-		if len(ss) == 0 {
+	for len(search) <= len(stack) {
+		if len(search) == 0 {
 			return true
 		}
-		if st[0].Name.Local == ss[0] {
-			ss = ss[1:]
+		if stack[0].Name.Local == search[0].Name {
+			search = search[1:]
 		}
-		st = st[1:]
+		stack = stack[1:]
 	}
 	return false
 }
