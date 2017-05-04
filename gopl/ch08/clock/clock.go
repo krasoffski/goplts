a33e7ffcc54460534b9efc337fa0fd236c065dcc
Yury Krasouski <krasoffski@gmail.com>
2017-05-04 21:47:50 +0300

    [8.1] add iface and port options

--- gopl/ch08/clock/clock.go
+++ gopl/ch08/clock/clock.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"time"
 )
 
+var (
+	iface, port string
+)
+
+func init() {
+	flag.StringVar(&iface, "iface", "localhost", "interface to listen")
+	flag.StringVar(&port, "port", "8000", "port to listen")
+	flag.Parse()
+}
+
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", iface, port))
 	if err != nil {
 		log.Fatal(err)
 	}
