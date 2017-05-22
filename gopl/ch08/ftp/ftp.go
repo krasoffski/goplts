bd09dd8c5cadbbb358cd9870436774f93ceabc65
Yury Krasouski <krasoffski@gmail.com>
2017-05-22 22:27:40 +0300

    [8.2] add draft for USER and PASS cmds

--- gopl/ch08/ftp/ftp.go
+++ gopl/ch08/ftp/ftp.go
@@ -2,10 +2,17 @@ package ftp
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"strings"
 )
+
+var users = map[string]string{
+	"anonymous":  "anonymous",
+	"krasoffski": "krasoffski",
+}
 
 func NewServer(address, path string) *Server {
 	return &Server{Addr: address, Path: path}
@@ -39,14 +46,60 @@ func NewHandler(conn net.Conn, path string) *Handler {
 
 type Handler struct {
 	Conn net.Conn
+	User string
 	Path string
+	Auth bool
 }
 
 func (h *Handler) Do() {
 	defer h.Conn.Close()
 
+	h.Message(220, "Welcome to Go language FTPd")
 	s := bufio.NewScanner(h.Conn)
 	for s.Scan() {
-		io.WriteString(h.Conn, s.Text()+"\r\n")
+		args := strings.SplitN(s.Text(), " ", 2)
+		h.handleCmd(args[0], args[1])
+	}
+}
+
+func (h *Handler) handleCmd(cmd, arg string) {
+	switch cmd {
+	case "USER":
+		h.HandleUSER(arg)
+	case "PASS":
+		h.HandlePASS(arg)
+	}
+}
+
+func (h *Handler) SendLine(text string) {
+	io.WriteString(h.Conn, text+"\r\n")
+}
+
+func (h *Handler) Message(code int, text string) {
+	h.SendLine(fmt.Sprintf("%d %s", code, text))
+}
+
+func (h *Handler) HandleUSER(name string) {
+	if h.User == "" {
+		if _, ok := users[name]; ok {
+			h.Message(331, fmt.Sprintf("User %s OK. Password required", name))
+			h.User = name
+		} else {
+			h.Message(530, "Login or password incorrect!")
+		}
+	} else {
+		// ???
+	}
+}
+
+func (h *Handler) HandlePASS(password string) {
+	if h.User != "" {
+		if p := users[h.User]; !h.Auth && password == p {
+			h.Message(230, fmt.Sprintf("Password is OK. Working directory %s", h.Path))
+		} else {
+			// ???
+		}
+	} else {
+		h.Message(530, "Login or password incorrect!")
 	}
 }
