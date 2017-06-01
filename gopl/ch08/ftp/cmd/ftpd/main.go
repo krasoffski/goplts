672e56a31a57608525ef58ca9d84a1319bb57b9d
Yury Krasouski <krasoffski@gmail.com>
2017-06-01 20:34:32 +0300

    [8.2] add flags for cli

--- gopl/ch08/ftp/cmd/ftpd/main.go
+++ gopl/ch08/ftp/cmd/ftpd/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/krasoffski/goplts/gopl/ch08/ftp"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/krasoffski/goplts/gopl/ch08/ftp"
+)
 
 func main() {
-	s := ftp.NewServer("localhost:9999", "/tmp")
+	host := flag.String("host", "0.0.0.0", "host to connect")
+	port := flag.String("port", "10021", "port to connect")
+	path := flag.String("path", ".", "initial working directory")
+	flag.Parse()
+
+	wd, err := filepath.Abs(*path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = os.Stat(wd)
+	if os.IsNotExist(err) {
+		log.Fatalln(err)
+	}
+
+	addr := fmt.Sprintf("%s:%s", *host, *port)
+	fmt.Printf("running at %s in %s\n", addr, wd)
+	s := ftp.NewServer(addr, wd)
 	s.Serve()
 }
