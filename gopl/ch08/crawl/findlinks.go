64ed9271bac641659e73c0c7919a39f1a2c567df
Yury Krasouski <krasoffski@gmail.com>
2017-08-04 10:45:02 +0300

    [8.6] add placeholder

---
+++ gopl/ch08/crawl/findlinks.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{}
+	list, err := Extract(url)
+	<-tokens
+
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+func main() {
+	worklist := make(chan []string)
+	var n int
+
+	n++
+	go func() { worklist <- os.Args[1:] }()
+
+	seen := make(map[string]bool)
+	for ; n > 0; n-- {
+		list := <-worklist
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				n++
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
+}
