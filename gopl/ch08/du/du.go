25dec1cb596f4400411023914d2b6385ff06985e
Yury Krasouski <krasoffski@gmail.com>
2017-08-08 19:50:30 +0300

    [8.9] add placeholder

---
+++ gopl/ch08/du/du.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+var (
+	done = make(chan struct{})
+	sema = make(chan struct{}, 20)
+)
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("%d file %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+func dirents(dir string) []os.FileInfo {
+	select {
+	case sema <- struct{}{}:
+	case <-done:
+		return nil
+	}
+	defer func() { <-sema }()
+
+	f, err := os.Open(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		return nil
+	}
+	defer f.Close()
+	entries, err := f.Readdir(0) // 0 => no limit; read all entries
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+	}
+	return entries
+}
+
+func walkDir(dir string, wg *sync.WaitGroup, fileSizes chan<- int64) {
+	defer wg.Done()
+
+	if cancelled() {
+		return
+	}
+
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			wg.Add(1)
+			subdir := filepath.Join(dir, entry.Name())
+			go walkDir(subdir, wg, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	roots := flag.Args()
+
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
+
+	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes)
+	}
+	go func() {
+		n.Wait()
+		close(fileSizes)
+	}()
+
+	tick := time.Tick(500 * time.Millisecond)
+	var nfiles, nbytes int64
+loop:
+	for {
+		select {
+		case <-done:
+			for range fileSizes {
+			}
+			return
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	printDiskUsage(nfiles, nbytes)
+}
