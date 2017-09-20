1d444960028077c9a5b726da69c6d403e8a2dc07
Yury Krasouski <krasoffski@gmail.com>
2017-09-20 22:09:27 +0300

    [8.14] removed logging

--- gopl/ch08/chat/chat.go
+++ gopl/ch08/chat/chat.go
@@ -24,17 +24,13 @@ var (
 func broadcaster() {
 	var online int
 	clients := make(map[client]bool)
-	log.Println("broadcaster started")
 	for {
 		select {
 		case msg := <-messages:
-			log.Println("start new broadcast message")
 			for cln := range clients {
 				cln.Chan <- "\t" + msg
 			}
-			log.Println("done new broadcast message")
 		case cln := <-entering:
-			log.Printf("entering %v", cln)
 			clients[cln] = true
 			online++
 			cln.Chan <- fmt.Sprintf("Online: %d", online)
@@ -45,13 +41,11 @@ func broadcaster() {
 			online--
 			delete(clients, cln)
 			close(cln.Chan)
-			log.Printf("left %v", cln)
 		}
 	}
 }
 
 func handleConn(conn net.Conn) {
-	log.Printf("handling conn for %s", conn.RemoteAddr().String())
 	rch := make(chan string)
 	wch := make(chan string)
 
@@ -66,7 +60,6 @@ func handleConn(conn net.Conn) {
 	entering <- cln
 
 	defer func() { // FIXME: don't like this solution
-		log.Printf("defer for %s", conn.RemoteAddr().String())
 		leaving <- cln
 		messages <- who + " has left"
 		close(rch)
@@ -76,12 +69,9 @@ func handleConn(conn net.Conn) {
 	for {
 		select {
 		case msg := <-rch:
-			log.Printf("message from %s", conn.RemoteAddr().String())
 			messages <- who + ": " + msg
 		case <-time.After(timeout):
-			// wch <- fmt.Sprintf("Inactivity more than %s.\nDisconnecting!\n",
-			// 	timeout)
-			log.Printf("inactivity for %s", conn.RemoteAddr().String())
+			// wch <- fmt.Sprintf("Inactivity more than %s!", timeout)
 			return
 		}
 	}
@@ -89,21 +79,16 @@ func handleConn(conn net.Conn) {
 
 func clientReader(conn net.Conn, ch chan<- string) {
 	// FIXME: Ignoring potential errors from input.Err()
-	log.Printf("reader start for %s", conn.RemoteAddr().String())
 	input := bufio.NewScanner(conn)
 	for input.Scan() {
 		ch <- input.Text()
 	}
-	log.Printf("reader stop for %s", conn.RemoteAddr().String())
 }
 
 func clientWriter(conn net.Conn, ch <-chan string) {
-	log.Printf("writer start for %s", conn.RemoteAddr().String())
 	for msg := range ch {
 		fmt.Fprintln(conn, msg)
-		log.Printf("send msg '%s' for %s", msg, conn.RemoteAddr().String())
 	}
-	log.Printf("writer stop for %s", conn.RemoteAddr().String())
 }
 
 func main() {
@@ -118,7 +103,6 @@ func main() {
 	go broadcaster()
 
 	for {
-		log.Println("waiting new client")
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
