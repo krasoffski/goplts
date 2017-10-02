a2d843d78c7a3888ba29e2e99a502bd8dab8003c
Yury Krasouski <krasoffski@gmail.com>
2017-10-02 20:52:21 +0300

    [9.1] add solution

--- gopl/ch09/bank/bank.go
+++ gopl/ch09/bank/bank.go
@@ -1,8 +1,14 @@
 package bank
 
+type Withdrawal struct {
+	amount  int
+	succeed chan bool
+}
+
 var (
-	deposits = make(chan int)
-	balances = make(chan int)
+	deposits    = make(chan int)
+	balances    = make(chan int)
+	withdrawals = make(chan Withdrawal)
 )
 
 func Deposit(amount int) {
@@ -13,12 +19,25 @@ func Balance() int {
 	return <-balances
 }
 
+func Withdraw(amount int) bool {
+	ch := make(chan bool)
+	withdrawals <- Withdrawal{amount, ch}
+	return <-ch
+}
+
 func teller() {
 	var balance int
 	for {
 		select {
 		case amount := <-deposits:
 			balance += amount
+		case w := <-withdrawals:
+			if w.amount > balance || w.amount <= 0 {
+				w.succeed <- false
+				continue
+			}
+			balance -= w.amount
+			w.succeed <- true
 		case balances <- balance:
 		}
 	}
