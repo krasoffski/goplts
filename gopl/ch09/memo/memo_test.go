6079600632f825412b291ecb8c3c822bbe4f13fd
Yury Krasouski <krasoffski@gmail.com>
2017-10-07 21:29:53 +0300

    [9.3] add draft test (not work)

--- gopl/ch09/memo/memo_test.go
+++ gopl/ch09/memo/memo_test.go
@@ -1,7 +1,13 @@
 package memo
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
+	"strings"
+	"sync"
+	"testing"
 	"time"
 )
 
@@ -11,11 +17,85 @@ type slowReader struct {
 }
 
 func (sr slowReader) Read(p []byte) (int, error) {
-	time.Sleep(sr.delay)
+	// time.Sleep(sr.delay)
 	return sr.r.Read(p[:1])
 }
 
-func NewReader(r io.Reader, bps int) io.Reader {
+func newReader(r io.Reader, bps int) io.Reader {
 	delay := time.Second / time.Duration(bps)
 	return slowReader{r: r, delay: delay}
+}
+
+func getSlowString(str string) (interface{}, error) {
+	s := strings.NewReader(str)
+	r := newReader(s, 10)
+	return ioutil.ReadAll(r)
+}
+
+var GetSlowString = getSlowString
+
+func incomingURLs() <-chan string {
+	ch := make(chan string)
+	go func() {
+		for _, url := range []string{
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+		} {
+			ch <- url
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+type M interface {
+	Get(key string) (interface{}, error)
+}
+
+func Sequential(t *testing.T, m M) {
+	for url := range incomingURLs() {
+		start := time.Now()
+		value, err := m.Get(url)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Printf("%s, %s, %d bytes\n",
+			url, time.Since(start), len(value.([]byte)))
+	}
+}
+
+func Concurrent(t *testing.T, m M) {
+	var n sync.WaitGroup
+	for url := range incomingURLs() {
+		n.Add(1)
+		go func(url string) {
+			defer n.Done()
+			start := time.Now()
+			value, err := m.Get(url)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			fmt.Printf("%s, %s, %d bytes\n",
+				url, time.Since(start), len(value.([]byte)))
+		}(url)
+	}
+	n.Wait()
+}
+
+func TestSequential(t *testing.T) {
+	m := New(getSlowString)
+	Sequential(t, m)
+}
+
+func TestConcurrent(t *testing.T) {
+	m := New(getSlowString)
+	Concurrent(t, m)
 }
