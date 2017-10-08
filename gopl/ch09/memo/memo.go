6ffd2094948092b48a325140c698ced25f099b3f
Yury Krasouski <krasoffski@gmail.com>
2017-10-08 14:37:30 +0300

    [9.3] updated types for solution

--- gopl/ch09/memo/memo.go
+++ gopl/ch09/memo/memo.go
@@ -1,7 +1,7 @@
 package memo
 
 // Func is the type of the function to memoize.
-type Func func(key string) (interface{}, error)
+type Func func(key string, done <-chan struct{}) (interface{}, error)
 
 type result struct {
 	value interface{}
@@ -16,31 +16,48 @@ type entry struct {
 type request struct {
 	key      string
 	response chan<- result
+	done     <-chan struct{} // cancellation channel
 }
 
 // Memo implements simple memoization of a function.
 type Memo struct {
-	requests chan request
+	requests, cancels chan request
 }
 
 // New initialized memoizarion structure.
 func New(f Func) *Memo {
-	memo := &Memo{requests: make(chan request)}
+	memo := &Memo{
+		requests: make(chan request),
+		cancels:  make(chan request),
+	}
 	go memo.server(f)
 	return memo
 }
 
 // Get calculates new result for Func or returns one
 // from the cache.
-func (m *Memo) Get(key string) (interface{}, error) {
+func (m *Memo) Get(key string, done <-chan struct{}) (interface{}, error) {
 	response := make(chan result)
-	m.requests <- request{key, response}
+	req := request{
+		key:      key,
+		response: response,
+		done:     done,
+	}
+	m.requests <- req
 	res := <-response
+	select {
+	case <-done:
+		m.cancels <- req
+	default:
+	}
 	return res.value, res.err
 }
 
 // Close closes internal channel for requests.
-func (m *Memo) Close() { close(m.requests) }
+func (m *Memo) Close() {
+	close(m.requests)
+	close(m.cancels)
+}
 
 func (m *Memo) server(f Func) {
 	cache := make(map[string]*entry)
@@ -49,14 +66,14 @@ func (m *Memo) server(f Func) {
 		if e == nil {
 			e = &entry{ready: make(chan struct{})}
 			cache[req.key] = e
-			go e.call(f, req.key)
+			go e.call(f, req.key, req.done)
 		}
 		go e.deliver(req.response)
 	}
 }
 
-func (e *entry) call(f Func, key string) {
-	e.res.value, e.res.err = f(key)
+func (e *entry) call(f Func, key string, done <-chan struct{}) {
+	e.res.value, e.res.err = f(key, done)
 	close(e.ready)
 }
 
