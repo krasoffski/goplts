283c3dbba50e9e083f937d1614900a60103afe65
Yury Krasouski <krasoffski@gmail.com>
2017-10-11 12:22:10 +0300

    [9.4] small fixes

--- gopl/ch09/pipes/pipes.go
+++ gopl/ch09/pipes/pipes.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	pipes := flag.Int("pipes", 10000, "number of sequential pipes")
+	pipes := flag.Int("pipes", 1000000, "number of sequential pipes")
+	verbose := flag.Bool("verbose", true, "interactive print number of pipes")
 	flag.Parse()
 
 	ch := make(chan struct{})
@@ -15,14 +16,15 @@ func main() {
 
 	for i := 1; i <= *pipes; i++ {
 		out := make(chan struct{})
+
 		go func(in <-chan struct{}, out chan<- struct{}, i int) {
 			out <- <-in
-			fmt.Printf("GO %10d\n", i)
 		}(in, out, i)
 
 		in = out
-
-		fmt.Printf("\r[%10d] ", i)
+		if *verbose {
+			fmt.Printf("\r[%10d] ", i)
+		}
 	}
 	start := time.Now()
 	ch <- struct{}{}
