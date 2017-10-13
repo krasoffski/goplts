1f63b1d4eadf0e18ce4f60e7f5fc4b5efae0377a
Yury Krasouski <krasoffski@gmail.com>
2017-10-13 13:20:47 +0300

    [9.5] one more approach with one select

--- gopl/ch09/pingpong/pinpong.go
+++ gopl/ch09/pingpong/pinpong.go
@@ -6,7 +6,9 @@ import (
 )
 
 func echo(in <-chan struct{}, out chan<- struct{}) {
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(1 * time.Second)
+	// defer ticker.Stop()
+
 	var i int
 	for {
 		// NOTE: add solution without select
@@ -25,7 +27,12 @@ func main() {
 	ch1 := make(chan struct{})
 	ch2 := make(chan struct{})
 	go echo(ch1, ch2)
-	go echo(ch2, ch1)
+	// go echo(ch2, ch1)
+	go func(in <-chan struct{}, out chan<- struct{}) {
+		for {
+			out <- <-in
+		}
+	}(ch2, ch1)
 	ch1 <- struct{}{}
 	select {}
 }
