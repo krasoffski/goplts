47db19711c2d2875778898def856aa114d152e2b
Yury Krasouski <krasoffski@gmail.com>
2017-10-13 13:07:31 +0300

    [9.5] replaced int to struct

--- gopl/ch09/pingpong/pinpong.go
+++ gopl/ch09/pingpong/pinpong.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-func echo(in <-chan int, out chan<- int) {
+func echo(in <-chan struct{}, out chan<- struct{}) {
 	ticker := time.NewTicker(time.Second)
 	var i int
 	for {
 		// NOTE: add solution without select
 		select {
 		case <-ticker.C:
-			fmt.Println(i)
+			fmt.Printf("\r%d ", i)
 			i = 0
 		default:
 			out <- <-in
@@ -22,10 +22,10 @@ func echo(in <-chan int, out chan<- int) {
 }
 
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+	ch1 := make(chan struct{})
+	ch2 := make(chan struct{})
 	go echo(ch1, ch2)
 	go echo(ch2, ch1)
-	ch1 <- 0
+	ch1 <- struct{}{}
 	select {}
 }
