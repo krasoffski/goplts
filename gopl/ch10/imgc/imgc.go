dbd6e8a3935015edaaa2a8ad5c41788baddada59
Yury Krasouski <krasoffski@gmail.com>
2017-10-15 12:46:21 +0300

    [10.1] add solution with swtich/case

--- gopl/ch10/imgc/imgc.go
+++ gopl/ch10/imgc/imgc.go
@@ -1,27 +1,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
-	_ "image/gif"
+	"image/gif"
 	"image/jpeg"
-	_ "image/png"
+	"image/png"
 	"io"
+	"log"
 	"os"
+	"strings"
 )
 
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix("imgc: ")
+}
+
 func main() {
-	if err := covert(os.Stdin, os.Stdout); err != nil {
-		fmt.Fprintf(os.Stderr, "imgc: %v\n", err)
-		os.Exit(1)
+	format := flag.String("format", "png",
+		"output format like ['gif', 'jpg', 'png")
+	flag.Parse()
+	*format = strings.ToLower(*format)
+
+	img, err := decode(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = encode(os.Stdout, img, *format)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func covert(in io.Reader, out io.Writer) error {
+func decode(in io.Reader) (image.Image, error) {
 	img, kind, err := image.Decode(in)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	fmt.Fprintln(os.Stderr, "Input format =", kind)
-	return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+	return img, nil
+}
+
+func encode(out io.Writer, img image.Image, kind string) (err error) {
+	// NOTE: can be implemented via map([string]convFunc)
+	switch kind {
+	case "jpg", "jpeg":
+		err = jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+	case "png":
+		err = png.Encode(out, img)
+	case "gif":
+		err = gif.Encode(out, img, &gif.Options{NumColors: 256})
+	}
+	return err
 }
