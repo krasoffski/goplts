5c604428a705c6b25933beedbb5a9df72d10b960
Yury Krasouski <krasoffski@gmail.com>
2017-11-16 18:28:48 +0300

    [11.1] add basic tests

--- gopl/ch04/charcount/charcount_test.go
+++ gopl/ch04/charcount/charcount_test.go
@@ -1,1 +1,22 @@
 package charcount
+
+import (
+	"strings"
+	"testing"
+)
+
+func fromString(str string) (map[string]int, error) {
+	return Counter(strings.NewReader(str))
+}
+
+func TestEmptyInputErr(t *testing.T) {
+	if _, err := fromString(""); err != nil {
+		t.Errorf("expected nil but got '%s'", err)
+	}
+}
+
+func TestEmptyInputResult(t *testing.T) {
+	if res, _ := fromString(""); len(res) != 0 && res != nil {
+		t.Errorf("expected empty map but got '%v'", res)
+	}
+}
